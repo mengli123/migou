6b6a6470bdf63f16ae359451d484735fcb5cb8e0
mengli123 <1084886813@qq.com>
2020-02-19 14:59:13 +0800

    修复信息

--- api/shop/controller/IndexController.php
+++ api/shop/controller/IndexController.php
@@ -10,14 +10,22 @@
 namespace api\shop\controller;
 
 use cmf\controller\RestBaseController;
+use think\Db;
 
 class IndexController extends RestBaseController
 {
+    /** 获取主轮播图*/
 	public function get_slide(){
-		echo 123;
+		$list=Db::name('slide_item')->where(['slide_id'=>1,'status'=>1])->field('title,image,url')->select();
+		if($list){
+            $this->success('请求成功!', $list);
+        }else{
+		    $this->error('请求失败');
+        }
 	}
     public function index()
     {
+        
         $this->success('请求成功!', ['test'=>'test']);
     }
 }
