5a558c97b3b2445b883645c8516dc6f41f86b502
mengli123 <1084886813@qq.com>
2020-03-03 20:33:22 +0800

    cat-game

--- api/shop/controller/CatController.php
+++ api/shop/controller/CatController.php
@@ -9,6 +9,17 @@ use think\Controller;
 
 class CatController extends RestBaseController
 {
+    //跨域方法
+    public function origin(){
+        // 指定允许其他域名访问
+        header('Access-Control-Allow-Origin:*');
+        // 响应类型
+        // header('Access-Control-Allow-Methods:POST');
+        header('Access-Control-Allow-Methods: OPTIONS,POST,GET');
+        // 响应头设置
+        header('Access-Control-Allow-Headers:origin,x-requested-with,content-type');
+
+    }
     /**
     猫咪列表
      */
@@ -23,6 +34,7 @@ class CatController extends RestBaseController
 
     /**
     领养猫咪
+     * 问题，可以同时喂养几只猫
      */
     public function buy_cat(){
         $user_id =input('user_id');
@@ -56,16 +68,24 @@ class CatController extends RestBaseController
     }
 
     /**
-    查询当前猫咪年龄段以及状态
+    查询当前5只猫咪年龄段以及状态
+     * 每一只距离上次喂养的时间
      */
-    public function cat_status(){
+    public function user_cat_status(){
         $user_id=input('user_id');
         if(!$user_id){
             $this->error('请传入user_id');
         }
-        $sel = Db::name('user_cat')->where(['user_id'=>$user_id])->select()->all();
-        if($sel){
-            $this->success('查询成功',$sel);
+        $sel = Db::name('user_cat')
+            ->where(['user_id'=>$user_id])
+            ->select()->all();
+        $user_cat_info=Db::name('user_cat_info')->where('user_id',$user_id)->find();
+        if($sel&&$user_cat_info){
+            $data=[
+                'info'=>$user_cat_info,
+                'cat'=>$sel
+            ];
+            $this->success('查询成功',$data);
         }else{
             $this->error('查询失败',[]);
         }
@@ -73,8 +93,33 @@ class CatController extends RestBaseController
 
     /**
     喂猫
+     * 问题：什么时候升级进入下一个阶段，需要一个事件去触发他
+     * 返回一个下次喂猫时间
      */
     public function feed_cat(){
+        $user_id=input('user_id');
+        $cat_id =input('cat_id');
+        $age_id =input('age_id');
+        $last_feed_time = Db::name('user_cat_log')->where(['user_id'=>$user_id,'cat_id'=>$cat_id])->value('ctime');
+        $interval =Db::name('cat_age')->where(['cat_id'=>$cat_id,'age_id'=>$age_id])->value('interval');
+        $feed_num =Db::name('cat_age')->where(['cat_id'=>$cat_id,'age_id'=>$age_id])->value('feed_num');
+        $duration=time()-$last_feed_time; //距上次喂猫过去了$interval秒
+        $will =$interval-$duration;  //$will秒后可以喂猫
+        if($duration<$interval){
+            $this->error('未到喂猫时间，请在'.$will.'秒后再来');
+        }
+        $data = [
+            'user_id'=>$user_id,
+            'cat_id'=>$cat_id,
+            'feed_num'=>$feed_num,
+            'ctime'=>time()
+        ];
+        $ins=Db::name('cat_age')->insert($data);
+        if($ins){
+            $this->success('喂猫成功');
+        }else{
+            $this->error('喂猫失败');
+        }
 
     }
 
