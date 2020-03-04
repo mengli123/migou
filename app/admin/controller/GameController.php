3f903f5743f17bdffe16f69141e63dfbd82fc401
mengli123 <1084886813@qq.com>
2020-03-04 12:07:40 +0800

    cat-admin

--- app/admin/controller/GameController.php
+++ app/admin/controller/GameController.php
@@ -17,6 +17,68 @@ class GameController extends AdminBaseController
 	}
 
 	/**
+	添加/修改猫咪列表
+     */
+	public function cat_add(){
+	    $cat_id=input('cat_id');
+	    if($cat_id!=''){
+	        /**修改猫咪*/
+            $cat =Db::name('cat')->where('cat_id',$cat_id)->find();
+            $this->assign('cat_id',$cat_id);
+            $this->assign('cat',$cat);
+        }
+	    return $this->fetch();
+    }
+    /**
+    保存猫咪数据
+     *
+     */
+    public function save_cat(){
+        $cat_id=input('cat_id');
+        $cat=input('cat');
+        $logo=input('logo');
+        $desc=input('desc');
+        if(!$cat){
+            $this->error('请输入猫名');
+        }
+        if(!$desc){
+            $this->error('请输入简介');
+        }
+        if(!$logo){
+            $this->error('请上传猫咪头像');
+        }
+        $data=[
+            'cat'=>$cat,
+            'logo'=>$logo,
+            'desc'=>$desc
+        ];
+        if($cat_id==''){
+            /**添加*/
+            $res=Db::name('cat')->insert($data);
+        }else{
+            /**修改*/
+            $res=Db::name('cat')->where('cat_id',$cat_id)->update($data);
+        }
+        if($res){
+            $this->success('保存成功','game/cat_list');
+        }else{
+            $this->error('保存失败','game/cat_list');
+        }
+    }
+    /**
+    删除猫咪
+     */
+    public function del_cat(){
+        $id = $this->request->param('cat_id', 0, 'intval');
+        $res=Db::name('cat')->where('cat_id',$id)->delete();
+        if($res !== false){
+            $this->success("删除成功！");
+        }else{
+            $this->error("删除失败！");
+        }
+    }
+
+	/**
 	管理阶段猫咪
      */
 	public function cat_age(){
@@ -91,7 +153,7 @@ class GameController extends AdminBaseController
         if ($res) {
             $this->success('保存成功', 'game/cat_list');
         }else{
-            $this->error('保存失败');
+            $this->error('保存失败','game/cat_list');
         }
     }
 
